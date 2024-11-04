cfb316d46ca3bb224433013c7b6f654d6f58b160
Nuno Tomé <nunotomeit@gmail.com>
2024-11-04 00:51:13 +0000

    Implement file saving and historic data management for API responses Some bugs are present

--- room2/worker_ipma_api_av_met_3.py
+++ room2/worker_ipma_api_av_met_3.py
@@ -56,6 +56,55 @@ DATASET_FILE_PATH = DATASET_FILE_NAME
 FILE_MAX_NUMBER_REGISTERS = 100 # 1MB
 # ---------------------------------------------------------------
 
+
+# Save the final file to the disk, replacing the oll one
+# to ensure that the information is not lost, keep a copy
+# of the old file in a historic folder
+# the file name should include the ordinal number 
+# so that the most recent file is always the one with the
+# highest number.
+#TODO: #9 change saving process to save the file with a new name
+def __save_api_data_to_file(purged_api_response):
+    try:
+        LOGGER.debug("Saving API data to file: %s", DATASET_FILE_PATH)
+        with open(DATASET_FILE_PATH, 'w', encoding='utf-8') as file:
+            file.write(json.dumps(purged_api_response))
+    except (IOError, OSError, json.JSONDecodeError) as e:
+        LOGGER.critical("Failed to save API data to file %s: %s", DATASET_FILE_NAME, e)
+        return
+ 
+    return
+    
+    
+def __add_api_response_to_file(api_response):
+
+    # check if the folder or file exists, if not, creates it and saves the 
+    # response as is and exits the function
+    if not os.path.exists(DATASET_FOLDER):
+        LOGGER.warning("Folder not found: %s", DATASET_FOLDER)
+        try:
+            os.makedirs(DATASET_FOLDER)
+            LOGGER.debug("Folder created: %s", DATASET_FOLDER)
+        except OSError as e:
+            LOGGER.critical("Failed to create folder %s: %s", DATASET_FOLDER, e)
+            return
+    if not os.path.exists(HISTORIC_FILE):
+        with open(HISTORIC_FILE, 'w', encoding='utf-8') as file:
+            file.write(json.dumps([api_response.text]))
+        return
+
+    # if the data file already exists, read it
+    with open(HISTORIC_FILE, 'r', encoding='utf-8') as file:
+        historic_data = json.loads(file.read())
+
+    # For the existing file, add JUST the new data to it adds the response
+    # to the existing data file don't add duplicated information
+    for register in api_response.text:
+        if register not in historic_data:
+            historic_data.append(register)
+
+    return historic_data
+
 #TODO: implement the following functions
 # Function to check if the purged response is empty
 def __purged_api_response_is_empty(purged_api_response):
@@ -82,16 +131,18 @@ def worker_ipma_api_av_met_3():
         
         
     # If the purged response is empty, ends the process
-    #TODO: verify if the purged response is empty
-    if not __purged_api_response_is_empty(api_response):
-        LOGGER.warning("Purged API response is empty, no need to save it to a file")
-  
-    # adds the response to the existing data file
-    #TODO: #5 implement the function to add the new info to the file
+    #TODO: #7 verify if the purged response is empty
+    if __purged_api_response_is_empty(api_response):
+        LOGGER.error("Purged API response is empty, no need to save it to a file")
+        return
+    LOGGER.debug("Purged API response is not empty")
+
+
+    #TODO: #8 implement the function to add the new info to the file
     # don't add duplicated information
     # limit the number of items in the file to a parameter
     # given in the configuration
-    
+    final_data = __add_api_response_to_file(api_response)
     
     # Save the final file to the disk, replacing the oll one
     # to ensure that the information is not lost, keep a copy
@@ -99,15 +150,11 @@ def worker_ipma_api_av_met_3():
     # the file name should include the ordinal number 
     # so that the most recent file is always the one with the
     # highest number.
-    #TODO: #6 change saving process to save the file with a new name
-    try:
-        LOGGER.debug("Saving API data to file: %s", DATASET_FILE_PATH)
-        with open(DATASET_FILE_PATH, 'w', encoding='utf-8') as file:
-            file.write(json.dumps(purged_api_response))
-    except (IOError, OSError, json.JSONDecodeError) as e:
-        LOGGER.critical("Failed to save API data to file %s: %s", DATASET_FILE_NAME, e)
-        return
- 
+    #TODO: #9 change saving process to save the file with a new name
+    __save_api_data_to_file(final_data)
+
+    pass
+
 # Test function
 # This is not going to used in production
 # FOR DEBUGGING ONLY
